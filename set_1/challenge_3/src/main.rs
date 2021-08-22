2d6cecd559bb523d22bc6ffb520bd4c9394c265e
Gary Hughes <garyedwardhughes@gmail.com>
2021-08-22 16:11:59 +1000

    Score the decoded strings and select the highest.

--- set_1/challenge_3/src/main.rs
+++ set_1/challenge_3/src/main.rs
@@ -4,13 +4,26 @@ use std::str;
 fn main() {
     let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     let bytes = hex::decode(input).unwrap();
-
+    let mut decoded: Vec<(char, Vec<u8>, u32)> = Vec::new();  
     for key in b'A' ..= b'z' {
-        let result_bytes: Vec<u8> = bytes.iter().map(|x| x ^ key).collect();
-        let result = str::from_utf8(&result_bytes).unwrap();
-        println!("{} {}", char::from(key), result);
+        let decoded_bytes: Vec<u8> = bytes.iter().map(|x| x ^ key).collect();
+        let score = score_bytes(&decoded_bytes); 
+        decoded.push((char::from(key), decoded_bytes, score));
     }
- 
+    decoded.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
+    match decoded.first() {
+        Some(result) => println!("{} {}", result.0, str::from_utf8(&result.1).unwrap()),
+        None => println!("No result found")
+    }
+}
 
+fn score_bytes(input: &Vec<u8>) -> u32 {
+    input.iter()
+        .map(|b| *b as char)
+        .map(|c| {
+            if c.is_alphanumeric() || c.is_whitespace() { 2 } 
+            else if c.is_ascii_punctuation() { 1 }
+            else { 0 }
+        }).sum()
 }
 
