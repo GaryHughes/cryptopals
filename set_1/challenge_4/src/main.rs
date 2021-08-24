048925a0498a743f01c67b20c9d1c8313fbe3eb3
Gary Hughes <garyedwardhughes@gmail.com>
2021-08-24 14:48:34 +1000

    Factor the text scoring function out into the library.

--- set_1/challenge_4/src/main.rs
+++ set_1/challenge_4/src/main.rs
@@ -2,18 +2,19 @@ use hex;
 use std::str;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use cryptopals::score_english_text;
 
 fn main() {
     let file = File::open("4.txt").unwrap();
     let reader = BufReader::new(file);
     // TODO - change this to just keep the best candidate
-    let mut decoded: Vec<(char, Vec<u8>, u32)> = Vec::new();
+    let mut decoded: Vec<(char, Vec<u8>, f64)> = Vec::new();
     for line in reader.lines() {
         let hex_line = line.unwrap();
         let bytes = hex::decode(hex_line).unwrap();
         for key in b'0' ..= b'z' {
             let decoded_bytes: Vec<u8> = bytes.iter().map(|x| x ^ key).collect();
-            let score = score_bytes(&decoded_bytes); 
+            let score = score_english_text(&decoded_bytes); 
             decoded.push((char::from(key), decoded_bytes, score));
         }
     }
@@ -22,14 +23,4 @@ fn main() {
         Some(result) => println!("{} {}", result.0, str::from_utf8(&result.1).unwrap()),
         None => println!("No result found")
     }
-}
-
-fn score_bytes(input: &Vec<u8>) -> u32 {
-    input.iter()
-        .map(|b| *b as char)
-        .map(|c| {
-            if c.is_alphanumeric() || c.is_whitespace() { 2 } 
-            else if c.is_ascii_punctuation() { 1 }
-            else { 0 }
-        }).sum()
 }
--- set_1/challenge_6/src/main.rs
+++ set_1/challenge_6/src/main.rs
@@ -1,9 +1,9 @@
 use base64;
-use std::collections::HashMap;
 use std::str;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use strided::Stride;
+use cryptopals::score_english_text;
 
 fn main() {
 
@@ -44,7 +44,7 @@ fn get_key(data: &[u8], key_size: usize) -> String {
         for i in 0..256 {
             let key = i as u8;
             let decoded_bytes: Vec<u8> = bytes.iter().map(|x| *x ^ key).collect();
-            let score = score_bytes(&decoded_bytes); 
+            let score = score_english_text(&decoded_bytes); 
             decoded.push((key, decoded_bytes, score));
         }
         decoded.sort_by(|a, b| b.2.partial_cmp(&a.2).unwrap());
@@ -55,52 +55,6 @@ fn get_key(data: &[u8], key_size: usize) -> String {
     }
 
     String::from_utf8(key).unwrap()
-}
-
-fn score_bytes(input: &Vec<u8>) -> f64 {
-   
-    // From https://en.wikipedia.org/wiki/Letter_frequency
-    // with the exception of ' ', which I estimated.
-    let mut character_frequencies: HashMap<char, f64> = HashMap::new();
-
-    character_frequencies.insert('a', 0.08167); 
-    character_frequencies.insert('b', 0.01492); 
-    character_frequencies.insert('c', 0.02782); 
-    character_frequencies.insert('d', 0.04253);
-    character_frequencies.insert('e', 0.12702);
-    character_frequencies.insert('f', 0.02228);
-    character_frequencies.insert('g', 0.02015); 
-    character_frequencies.insert('h', 0.06094);
-    character_frequencies.insert('i', 0.06094);
-    character_frequencies.insert('j', 0.00153);
-    character_frequencies.insert('k', 0.00772);
-    character_frequencies.insert('l', 0.04025);
-    character_frequencies.insert('m', 0.02406);
-    character_frequencies.insert('n', 0.06749);
-    character_frequencies.insert('o', 0.07507); 
-    character_frequencies.insert('p', 0.01929);
-    character_frequencies.insert('q', 0.00095); 
-    character_frequencies.insert('r', 0.05987); 
-    character_frequencies.insert('s', 0.06327); 
-    character_frequencies.insert('t', 0.09056);
-    character_frequencies.insert('u', 0.02758); 
-    character_frequencies.insert('v', 0.00978); 
-    character_frequencies.insert('w', 0.02360); 
-    character_frequencies.insert('x', 0.00150);
-    character_frequencies.insert('y', 0.01974);
-    character_frequencies.insert('z', 0.00074); 
-    character_frequencies.insert(' ', 0.13000);
-  
-    input.iter()
-        .map(|b| *b as char)
-        .map(|c| {
-            let lower = c.to_lowercase().collect::<Vec<_>>()[0];
-            match character_frequencies.get(&lower) {
-                Some(f) => *f,
-                None => 0.0
-            }
-        })
-        .sum()
 }
 
 fn get_key_size(data: &[u8]) -> usize {
